6cb142184b0d033c0ad89a3e1364f0182f6cde86
Felipe Bergamin <felipebergamin6@gmail.com>
2020-07-26 12:24:33 -0300

    setup project tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,44 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-native', 'react-hooks'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'consistent-return': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react-native/no-unused-styles': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'import/extensions': [
+      'error',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'always',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
